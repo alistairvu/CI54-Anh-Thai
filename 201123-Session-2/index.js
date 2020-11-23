0261757e813a2487d3dcb431ff186b7680135b78
Alistair Vu <alistairvu@gmail.com>
2020-11-23 20:57:28 +0700

    Add static methods and class inheritance

--- 201123-Session-2/index.js
+++ 201123-Session-2/index.js
@@ -31,3 +31,47 @@ const yuju = new Person("Yuju", 20, "Hanoi", "female")
 // const abc = new Person("abc", 3, "Hanoi", "male")
 console.log(yuju)
 yuju.intro()
+
+// Static methods
+
+class MyMath {
+  static sum(a, b) {
+    return a + b
+  }
+
+  static div(a, b) {
+    return a / b
+  }
+}
+
+// console.log(MyMath.sum(1, 2))
+
+// class inheritance
+class Employee extends Person {
+  constructor(name, age, address, gender, salary) {
+    super(name, age, address, gender)
+    this.salary = salary
+  }
+
+  intro() {
+    this.name.toLowerCase() === "trump" &&
+      console.log(
+        "I WON THE ELECTION!!! JOE BIDEN STOLE THE ELECTION FROM ME!!!!!!!"
+      )
+  }
+
+  showOff() {
+    this.name.toLowerCase() === "irene"
+      ? console.log("I'M A LITTLE MONSTER!!!!")
+      : console.log(
+          `I EARN $${this.salary} A YEAR JUST BY BREATHING!!!!!111!!!!`
+        )
+  }
+}
+
+const notMyPresident = new Employee("Trump", 76, "Washington", "male", 2)
+console.log(notMyPresident)
+notMyPresident.intro()
+notMyPresident.showOff()
+const irene = new Employee("Irene", 30, "Seoul", "female", 3)
+irene.showOff()
